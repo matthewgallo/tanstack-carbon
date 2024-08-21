1aecdf38a3a8f4b69463fb774d33675ebc3e3d2c
Matt Gallo <mdgallo@us.ibm.com>
2024-08-21 16:11:31 -0400

    initial

---
+++ src/App.tsx
@@ -0,0 +1,206 @@
+import * as React from 'react'
+import { DataTable } from '@carbon/react';
+const {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableHeader,
+  TableRow
+} = DataTable;
+
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  ColumnResizeMode
+} from '@tanstack/react-table'
+
+type Resource = {
+  id: string
+  name: string
+  rule: string
+  status: string
+  other: string
+  example: string
+}
+
+const defaultData: Resource[] = [
+  {
+    id: 'load-balancer-1',
+    name: 'Load Balancer 1',
+    rule: 'Round robin',
+    status: 'Starting',
+    other: 'Test',
+    example: '22',
+  },
+  {
+    id: 'load-balancer-2',
+    name: 'Load Balancer 2',
+    rule: 'DNS delegation',
+    status: 'Active',
+    other: 'Test',
+    example: '22',
+  },
+  {
+    id: 'load-balancer-3',
+    name: 'Load Balancer 3',
+    rule: 'Round robin',
+    status: 'Disabled',
+    other: 'Test',
+    example: '22',
+  },
+  {
+    id: 'load-balancer-4',
+    name: 'Load Balancer 4',
+    rule: 'Round robin',
+    status: 'Disabled',
+    other: 'Test',
+    example: '22',
+  },
+  {
+    id: 'load-balancer-5',
+    name: 'Load Balancer 5',
+    rule: 'Round robin',
+    status: 'Disabled',
+    other: 'Test',
+    example: '22',
+  },
+  {
+    id: 'load-balancer-6',
+    name: 'Load Balancer 6',
+    rule: 'Round robin',
+    status: 'Disabled',
+    other: 'Test',
+    example: '22',
+  },
+  {
+    id: 'load-balancer-7',
+    name: 'Load Balancer 7',
+    rule: 'Round robin',
+    status: 'Disabled',
+    other: 'Test',
+    example: '22',
+  },
+];
+
+const columnHelper = createColumnHelper<Resource>()
+
+const columns = [
+  columnHelper.accessor(row => row.name, {
+    id: 'name',
+    cell: info => <i>{info.getValue()}</i>,
+    header: () => <span>Name</span>,
+  }),
+  columnHelper.accessor('rule', {
+    header: () => 'Rule',
+    cell: info => info.renderValue(),
+  }),
+  columnHelper.accessor('status', {
+    header: () => <span>Status</span>,
+  }),
+  columnHelper.accessor('other', {
+    header: 'Other',
+  }),
+  columnHelper.accessor('example', {
+    header: 'Example',
+  }),
+]
+
+export const App = () => {
+  const [data] = React.useState(() => [...defaultData])
+
+  const [columnResizeMode] = React.useState<ColumnResizeMode>('onChange')
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    columnResizeMode
+  })
+
+  return (
+    <TableContainer
+      title="Basic tanstack / carbon react table"
+      className="basic-table"
+      style={{
+        width: table.getCenterTotalSize(),
+      }}
+    >
+      <Table
+        size="lg"
+        useZebraStyles={false}
+        aria-label="sample table"
+        style={{
+          width: table.getCenterTotalSize(),
+        }}
+      >
+        <TableHead>
+          {table.getHeaderGroups().map(headerGroup => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map(header => (
+                <TableHeader
+                  key={header.id}
+                  style={{
+                    width: header.getSize(),
+                  }}
+                >
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                    <div
+                        {...{
+                          onDoubleClick: () => header.column.resetSize(),
+                          onMouseDown: header.getResizeHandler(),
+                          onTouchStart: header.getResizeHandler(),
+                          className: `resizer ${
+                            table.options.columnResizeDirection
+                          } ${
+                            header.column.getIsResizing() ? 'isResizing' : ''
+                          }`,
+                          style: {
+                            transform:
+                              columnResizeMode === 'onEnd' &&
+                              header.column.getIsResizing()
+                                ? `translateX(${
+                                    (table.options.columnResizeDirection ===
+                                    'rtl'
+                                      ? -1
+                                      : 1) *
+                                    (table.getState().columnSizingInfo
+                                      .deltaOffset ?? 0)
+                                  }px)`
+                                : '',
+                          },
+                        }}
+                      />
+                </TableHeader>
+              ))}
+            </TableRow>
+          ))}
+        </TableHead>
+        <TableBody>
+          {table.getRowModel().rows.map(row => (
+            <TableRow key={row.id}>
+              {row.getVisibleCells().map(cell => (
+                <TableCell
+                  key={cell.id}
+                  style={{
+                    width: cell.column.getSize(),
+                  }}
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
