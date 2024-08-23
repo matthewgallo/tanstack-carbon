2f69269ea6bea0e4418ca60dc0adc5aaf1e6045c
Matt Gallo <mdgallo@us.ibm.com>
2024-08-23 14:39:03 -0400

    fix: disable row condition

--- src/WithBatchActions.tsx
+++ src/WithBatchActions.tsx
@@ -169,7 +169,7 @@ export const WithBatchActions = () => {
       fuzzy: fuzzyFilter, //define as a filter function that can be used in column definitions
     },
     // enableRowSelection: true, //enable row selection for all rows
-    enableRowSelection: row => row.original.status === 'disabled', // conditionally disable rows
+    enableRowSelection: row => row.original.status !== 'disabled', // conditionally disable rows
     // enableRowSelection: row => row.original.age > 18, // or enable row selection 
     onRowSelectionChange: setRowSelection,
   })
